8cbbecff04b7c83dec5bc950abd8f7f7035768f7
joel alexander <wksx9d@gmail.com>
2025-11-15 18:29:15 -0500

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import dataProvider from "@refinedev/simple-rest";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { authProvider } from "./authProvider";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerProvider}
+                authProvider={authProvider}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  projectId: "ae5xBd-1su0pE-uFHGjn",
+                }}
+              >
+                <Routes>
+                  <Route index element={<WelcomePage />} />
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
